5e794f6e050bbd89fbdfab69950ac810bae264a1
danielmaica <danielmaica99@gmail.com>
2023-04-19 21:19:56 -0300

    js moderno

---
+++ js3-moderno/index.js
@@ -0,0 +1,37 @@
+// Template Literals
+// const name = 'Isaac'
+// console.log(`Olá, ${name}
+// Seja bem-vindo!
+// Estamos em ${Date()}.`)
+
+// Arrow Function
+// const sum = (a, b) => a + b
+// const multi = (a, b) => a * b
+// console.log(sum(3, 5))
+// console.log(multi(3, 5))
+// const towns = ['Porto Alegre', 'São Paulo', 'Brasilia', 'Rio de Janeiro']
+// const startingWithP = towns.filter(town => town[0] === 'P')
+// console.log(startingWithP)
+
+// Desestruturação de objetos e arrays
+// const person = {
+//   name: 'Luke',
+//   job: 'Farmer',
+//   parents: ['Anakin', 'Padme']
+// }
+// const name = person.name
+// const { job, parents } = person
+// console.log(name, job, parents)
+// const [father, mother] = parents
+// console.log(father, mother)
+// function createUser({name, job, parents}){
+//   const id = Math.floor(Math.random() * 9999)
+//   return {
+//     id,
+//     name: name,
+//     job: job,
+//     parents: parents
+//   }
+// }
+// const luke = createUser(person)
+// console.log(luke)
